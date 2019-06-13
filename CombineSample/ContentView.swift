6772977c52a40f67b05b323a4823b48bbbf30eeb
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-06-13 11:09:41 +0900

    Initial Commit

---
+++ CombineSample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CombineSample
+//
+//  Created by akifumi.fukaya on 2019/06/13.
+//  Copyright © 2019 Akifumi Fukaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
