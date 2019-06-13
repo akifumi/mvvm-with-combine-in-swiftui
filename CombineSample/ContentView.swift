924d3e314dbc3db308e22efeadb3fdce25f721cf
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-06-13 11:15:48 +0900

    Created ContentViewModel.

--- CombineSample/ContentView.swift
+++ CombineSample/ContentView.swift
@@ -7,6 +7,65 @@
 //
 
 import SwiftUI
+import Combine
+
+final class ContentViewModel : BindableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    var username: String = "" {
+        didSet {
+            guard oldValue != username else { return }
+            usernameSubject.send(username)
+            didChange.send(())
+        }
+    }
+    struct StatusText {
+        let content: String
+        let color: Color
+    }
+    var status: StatusText = StatusText(content: "NG", color: .red) {
+        didSet {
+            didChange.send(())
+        }
+    }
+    private let usernameSubject = PassthroughSubject<String, Never>()
+    private var validatedUsername: AnyPublisher<String?, Never> {
+        return usernameSubject
+//            .debounce(for: 0.5, scheduler: RunLoop.main)
+//            .removeDuplicates()
+            .flatMap { (username) -> AnyPublisher<String?, Never> in
+                Publishers.Future<String?, Never> { (promise) in
+                    // FIXME: API request
+                    if 1...10 ~= username.count {
+                        promise(.success(username))
+                    } else {
+                        promise(.success(nil))
+                    }
+                }
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    lazy var onAppear: () -> Void = { [weak self] in
+        _ = self?.validatedUsername
+            .sink(receiveValue: { (value) in
+                if let value = value {
+                    self?.username = value
+                } else {
+                    print("validatedUsername.receiveValue: Invalid username")
+                }
+            })
+
+        // Update StatusText
+        _ = self?.validatedUsername
+            .map { (value) -> StatusText in
+                (value != nil) ? StatusText(content: "OK", color: .green) : StatusText(content: "NG", color: .red)
+            }
+            .sink(receiveValue: { [weak self] (value) in
+                self?.status = value
+            })
+    }
+}
 
 struct ContentView : View {
     @State private var username: String = ""
